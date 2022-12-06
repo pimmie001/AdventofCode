107ad9609caef30739ca183c385404651bdb84b2
pimmie001 <pimvkeulen@gmail.com>
2022-12-06 10:47:48 +0100

    2022 day 6

---
+++ 2022/6.py
@@ -0,0 +1,9 @@
+with open('6.txt') as fh:
+    string = fh.read()
+
+n = 14 # choose 4 or 14 for part 1/2
+for i in range(len(string)-n):
+    substring = string[i:i+n]
+    if len(substring) == len(set(substring)):
+        print(i+n) # part 1: 1848
+        break
