5a0724e460f9fa70412cb0838b82ad3078a872b3
pimmie001 <pimvkeulen@gmail.com>
2022-12-06 10:50:11 +0100

    2022 day 6

--- 2022/6.py
+++ 2022/6.py
@@ -5,5 +5,5 @@ n = 14 # choose 4 or 14 for part 1/2
 for i in range(len(string)-n):
     substring = string[i:i+n]
     if len(substring) == len(set(substring)):
-        print(i+n) # part 1: 1848
+        print(i+n) # part 1: 1848, part 2: 2308
         break
