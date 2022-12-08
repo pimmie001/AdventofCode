68737d658b075d11aa8aabba384f19c15f8c5070
pimmie001 <pimvkeulen@gmail.com>
2022-12-08 12:43:01 +0100

    2022 day 8.2

--- 2022/8.py
+++ 2022/8.py
@@ -18,19 +18,41 @@ def max(A):
     return np.max(A)
 
 
-
 total = 0
 for i in range(np.shape(grid)[0]):
     for j in range(np.shape(grid)[1]):
-        height = grid[i, j]
-
-        visible = False
-        if min([max(grid[:i, j]), max(grid[i+1: ,j]), max(grid[i, :j]), max(grid[i, j+1:])]) < height:
-            visible = True
-
-        total += visible
-
+        total += min([max(grid[:i, j]), max(grid[i+1: ,j]), max(grid[i, :j]), max(grid[i, j+1:])]) < grid[i, j]
 print(total) # part 1: 1533
 
 
+### part 2:
 
+def scenic_score(i, j):
+    counts = []
+    for line in [reversed(grid[:i, j]), grid[i+1:, j], reversed(grid[i, :j]), grid[i, j+1:]]:
+        counts.append(visible_trees(grid[i, j], line))
+
+    return np.prod(counts)
+
+
+def visible_trees(own_height, line):
+    count = 0
+    max_tree = -1
+    for x in line:
+        if x < own_height and x >= max_tree:
+            count += 1
+
+        if x >= own_height:
+            count += 1
+            break
+
+    return count
+
+
+best_score = 0
+for i in range(1, np.shape(grid)[0]-1):
+    for j in range(1, np.shape(grid)[1]-1):
+        score = scenic_score(i, j)
+        if score > best_score:
+            best_score = score
+print(best_score) # 345744
