e96c2ad557652501313c1bebc71ce2a069c7ad90
pimmie001 <pimvkeulen@gmail.com>
2022-12-09 11:49:17 +0100

    2022 day 9.2

--- 2022/9.py
+++ 2022/9.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 with open ('9.txt') as fh:
     instructions = fh.read().split('\n')
 
@@ -60,3 +62,32 @@ for instruction in instructions:
 print(len(tail_visited)) # part 1: 6494
 
 
+
+### part 2:
+num = 10 # 9 tails + head
+locations = np.zeros((num, 2))
+
+last_tail_visited = set()
+
+
+for instruction in instructions:
+    direction, n = instruction.split(' ')
+    n = int(n)
+    
+    if direction == 'R':
+        dx, dy = 1, 0
+    elif direction == 'L':
+        dx, dy = -1, 0
+    elif direction == 'U':
+        dx, dy = 0, 1
+    else:
+        dx, dy = 0, -1
+
+    for i in range(n):
+        locations[0, :] = move_head(locations[0, 0], locations[0, 1], dx, dy)
+        for tail in range(1, num):
+            locations[tail, :] = move_tail(locations[tail-1, 0], locations[tail-1, 1], locations[tail, 0], locations[tail, 1])
+
+        last_tail_visited.add((locations[-1, 0], locations[-1, 1]))
+
+print(len(last_tail_visited)) # 2691
