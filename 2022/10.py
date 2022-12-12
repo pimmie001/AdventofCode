b3c6ea0bfbd1abd3957710d74b1e0b8bbb21eadc
pimmie001 <pimvkeulen@gmail.com>
2022-12-12 13:22:34 +0100

    2022 day 10.2

--- 2022/10.py
+++ 2022/10.py
@@ -10,9 +10,6 @@ n = None
 i = 0
 cycle = 1
 while cycle <= cycles[-1]:
-    if cycle in cycles:
-        Values.append(X*cycle)
-
     if n:
         X += n
         cycle += 1
@@ -26,8 +23,46 @@ while cycle <= cycles[-1]:
     else: 
         n = None
 
-    i = (i+1)%len(instructions)
+    i += 1
     cycle += 1
+
+    if cycle in cycles:
+        Values.append(X*cycle)
 
 print(sum(Values)) # part 1: 15220
 
+
+### part 2:
+X = 1
+n = None
+i = 0
+cycle = 0
+while cycle < 6*40:
+    if cycle % 40 == 0:
+        print()
+    if abs(cycle % 40 - X) <= 1:
+        print('#', end='')
+    else:
+        print('.', end='')
+
+    if n:
+        X += n
+        cycle += 1
+
+        if cycle % 40 == 0:
+            print()
+        if abs(cycle % 40 - X) <= 1:
+            print('#', end='')
+        else:
+            print('.', end='')
+
+    instruction = instructions[i]
+    if 'addx' in instruction:
+        n = int(instruction.split(' ')[1])
+    else: 
+        n = None
+
+    i += 1
+    cycle += 1
+
+# part 2: RFZEKBFA
