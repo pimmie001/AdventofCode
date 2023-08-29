67bef4664fa305c32b078ff01e938c52bdc73ad1
Pim <pimvkeulen@gmail.com>
2023-08-29 21:41:09 +0200

    2017 day 8.2

--- 2017/8.cpp
+++ 2017/8.cpp
@@ -20,6 +20,7 @@ int main() {
     ifstream file("8.txt");
 
     map<string, int> values;
+    int all_time_max = 0;
 
     string line;
     while (getline(file, line)) {
@@ -53,6 +54,9 @@ int main() {
         if (success) {
             if (words[1] == "inc") {
                 values[words[0]] = get_value(words[0], values) + stoi(words[2]);
+                if (values[words[0]] > all_time_max) { // for part 2
+                    all_time_max = values[words[0]];
+                }
             }
             else {
                 values[words[0]] = get_value(words[0], values) - stoi(words[2]);
@@ -70,6 +74,7 @@ int main() {
         }
     }
     cout << maximum << endl; // part 1: 4066
+    cout << all_time_max << endl; //part 2: 4829
 
     return 0;
 }
