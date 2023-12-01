0da92d4aefe1ae6590717a084804e7a2728327e6
pimmie001 <pimvkeulen@gmail.com>
2023-12-01 12:31:54 +0100

    2023: day 1.2

--- 2023/1.py
+++ 2023/1.py
@@ -1,3 +1,6 @@
+import re
+
+
 with open('1.txt') as fh:
     content = fh.read().split('\n')
 
@@ -19,3 +22,33 @@ for line in content:
         total += int(first+first)
 
 print(total) # part 1: 55447
+
+
+
+number_dict = {'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
+
+def extract_numbers(string):
+    result = []
+
+    for i in range(len(string)):
+        if string[i].isnumeric():
+            result.append(string[i])
+            continue
+
+        for word in number_dict:
+            if string[i:i+len(word)] == word:
+                result.append(number_dict[word])
+                continue
+
+    return result
+
+
+total2 = 0
+for line in content:
+    numbers = extract_numbers(line)
+    if len(numbers) == 1:
+        numbers *= 2
+
+    total2 += int(numbers[0] + numbers[-1])
+
+print(total2) # part 2: 54706
