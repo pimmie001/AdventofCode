3eb88598730ef1340ab76bab3f98541f2548e61d
pimmie001 <pimvkeulen@gmail.com>
2023-12-03 23:28:09 +0100

    2023 day 3.2

--- 2023/3.py
+++ 2023/3.py
@@ -31,7 +31,7 @@ for y in range(n):
             x_end = (x-1)
 
             if number:
-                numbers.append(number)
+                numbers.append(int(number))
                 coordinates.append((x_start, x_end, y_coor))
 
             number = ''
@@ -56,5 +56,43 @@ def valid(coordinates):
 total = 0
 for i in range(len(numbers)):
     if valid(coordinates[i]):
-        total += int(numbers[i])
+        total += numbers[i]
 print(total) # part 1: 520019
+
+
+
+###### part 2
+
+## find coordinates gear
+coordinates_gear = []
+for y in range(n):
+    for x in range(m):
+        char = content[y][x]
+        if char == '*':
+            coordinates_gear.append((x,y))
+
+
+## find gear ratio
+def gear_ratio(x_g, y_g):
+    A = []
+    for i in range(len(numbers)):
+        x_start, x_end, y_coor = coordinates[i]
+
+        adjacents = []
+        for x in range(x_start-1, x_end+2):
+            for y in range(y_coor-1, y_coor+2):
+                adjacents.append((x,y))
+
+        if (x_g, y_g) in adjacents:
+            A.append(numbers[i])
+
+    if len(A) < 2:
+        return 0
+    return A[0] * A[1]
+
+
+## get total
+total2 = 0
+for (x_g, y_g) in coordinates_gear:
+    total2 += gear_ratio(x_g, y_g)
+print(total2) # part 2: 75519888
