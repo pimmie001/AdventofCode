337a64200b90c350fce240d5501a538f36235d7b
pimmie001 <pimvkeulen@gmail.com>
2023-12-11 17:45:06 +0100

    2023 day 11 part 2

--- 2023/11.py
+++ 2023/11.py
@@ -1,5 +1,6 @@
 with open('11.txt') as fh:
     grid = fh.read().split('\n')
+    grid2 = grid.copy() # for part 2
 
 n = len(grid)
 m = len(grid[0])
@@ -62,3 +63,47 @@ for i in range(len(coors)):
         x2,y2 = coors[j]
         total += abs(x1-x2) + abs(y1-y2)
 print(total) # part 1: 9329143
+
+
+##### part 2
+x_expand = set(x_expand)
+y_expand = set(y_expand)
+
+def get_distance(x1,y1,x2,y2, M=1_000_000):
+    xsmall = min(x1,x2)
+    xlarge = max(x1,x2)
+    ysmall = min(y1,y2)
+    ylarge = max(y1,y2)
+
+    dist = 0
+    for x in range(xsmall+1, xlarge+1):
+        if x in x_expand:
+            dist += M
+        else: 
+            dist += 1
+    for y in range(ysmall+1, ylarge+1):
+        if y in y_expand:
+            dist += M
+        else:
+            dist += 1
+
+    return dist
+
+
+## get coordinates on standard grid
+n = len(grid2)
+m = len(grid2[0])
+coors2 = []
+for y in range(n):
+    for x in range(m):
+        if grid2[y][x] == '#':
+            coors2.append((x,y))
+
+## sum distance of all pairs
+total = 0
+for i in range(len(coors2)):
+    x1,y1 = coors2[i]
+    for j in range(i+1, len(coors2)):
+        x2,y2 = coors2[j]
+        total += get_distance(x1,y1,x2,y2)
+print(total) # part 2: 710674907809
