c5996e890c41c2b342764a992dfd7905eb792b53
pimmie001 <pimvkeulen@gmail.com>
2024-12-07 21:20:33 +0100

    2024 day 5.2

--- 2024/5.py
+++ 2024/5.py
@@ -25,8 +25,38 @@ def validate_sequence(sequence, rules=rules):
 
 # count correct sequences
 total = 0
+incorrect_sequences = []
 for x in page_numbers:
     seq = [int(y) for y in x.split(',')]
-    total += seq[len(seq)//2] * validate_sequence(seq)
+
+    if validate_sequence(seq):
+        total += seq[len(seq)//2]
+    else: # for part 2
+        incorrect_sequences.append(seq)
 
 print(total) # part 1: 5991 
+
+
+
+def find_incorrect(sequence, rules=rules):
+    index_map = {element: i for i, element in enumerate(sequence)}
+
+    # Check each rule
+    for x, y in rules:
+        if index_map.get(x, -1) > index_map.get(y, float('inf')):
+            return index_map[x], index_map[y]
+    return True
+
+
+total2 = 0
+for seq in incorrect_sequences:
+    result = result = find_incorrect(seq)
+
+    while result != True: # simple bubble sort to put in correct order
+        i,j = result
+        seq[i], seq[j] = seq[j], seq[i]
+        result = find_incorrect(seq)
+
+    total2 += seq[len(seq)//2]
+
+print(total2) # part 2: 5479
