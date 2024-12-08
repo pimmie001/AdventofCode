4ca7a273149d2f4ba693a6d81aadd27cdc9de801
Pim van Keulen <pimvkeulen@gmail.com>
2024-12-08 13:01:47 +0100

    2024 day 7.2

--- 2024/7.py
+++ 2024/7.py
@@ -29,3 +29,24 @@ for i in range(len(A)):
     if is_valid(A[i], B[i][::-1]):
         total += A[i]
 print(total) # part 1: 4364915411363
+
+
+def is_valid2(a, b):
+    """Same as before but also can concatenate numbers"""
+
+    if len(b) == 1:
+        return a == b[0]
+
+    if b[-1] > a:
+        return False
+
+    n = b.pop()
+    m = b.pop()
+    return is_valid2(a, b + [n+m]) or is_valid2(a, b + [n*m]) or is_valid2(a, b + [int(str(n)+str(m))])
+
+
+total2 = 0
+for i in range(len(A)):
+    if is_valid2(A[i], B[i][::-1]):
+        total2 += A[i]
+print(total2) # part 2: 38322057216320
