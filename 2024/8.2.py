98ccf882422c80b336c35f090a5031bf2849b42d
Pim van Keulen <pimvkeulen@gmail.com>
2024-12-08 14:05:09 +0100

    2024 day 8.2

---
+++ 2024/8.2.py
@@ -0,0 +1,42 @@
+with open("8.txt") as fh:
+    grid = fh.read().split('\n')
+    m = len(grid)
+    n = len(grid[0])
+
+frequencies = {}
+for y in range(m):
+    for x in range(n):
+        char = grid[y][x]
+        if char != '.':
+            if char in frequencies:
+                frequencies[char].append((x,y))
+            else:
+                frequencies[char] = [(x,y)]
+
+
+anti_node_locations = set()
+for x in frequencies:
+    locations = frequencies[x] # locations of antenna x
+
+    for i in range(len(locations)):
+        x1,y1 = locations[i]
+        for j in range(i+1, len(locations)):
+            x2,y2 = locations[j]
+
+            # add while within map
+            x1_new, y1_new = x1,y1
+            x2_new, y2_new = x2,y2
+            while x1_new >= 0 and x1_new < n and y1_new >= 0 and y1_new < m:
+                anti_node_locations.add((x1_new, y1_new))
+
+                x1_new -= (x2-x1)
+                y1_new -= (y2-y1)
+
+            while x2_new >= 0 and x2_new < n and y2_new >= 0 and y2_new < m:
+                anti_node_locations.add((x2_new, y2_new))
+
+                x2_new += (x2-x1)
+                y2_new += (y2-y1)
+
+
+print(len(anti_node_locations)) # part 2: 1280
