7989ea46f7c3d4049d633084b70d43367e7b0e52
Pim van Keulen <pimvkeulen@gmail.com>
2024-12-08 14:45:04 +0100

    2024 simplified 4.2

--- 2024/4.2.py
+++ 2024/4.2.py
@@ -3,20 +3,12 @@ import numpy as np
 with open("4.txt") as fh:
     grid = fh.read().split('\n')
 
-
-## add layers of 0's so we dont get index out of bound errors
-A = [list('000'+x+'000') for x in grid]
-for _ in range(3):
-    A.insert(0, ['0']*len(A[0]))
-    A.append(['0']*len(A[0]))
-
-
-grid = np.array([list(x) for x in A])
+grid = np.array([list(x) for x in grid])
 
 
 count = 0
-for y in range(3, grid.shape[0]-3):
-    for x in range(3, grid.shape[1]-3):
+for y in range(grid.shape[0]-2):
+    for x in range(grid.shape[1]-2):
 
         diagonal = np.diag(grid[y:y+3, x:x+3])
         anti_diagonal = np.flipud(grid[y:y+3, x:x+3]).diagonal()
