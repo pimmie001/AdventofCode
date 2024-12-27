ae455b389275dca6dd24682e5424e7935e60422c
Pim van Keulen <pimvkeulen@gmail.com>
2024-12-27 15:58:31 +0100

    2024 day 18.2

--- 2024/18.py
+++ 2024/18.py
@@ -20,25 +20,38 @@ def get_neighbors(x,y, memory_space=memory_space):
     return neighbors
 
 
-## Dijkstra
+def dijkstra(memory_space):
+    dist = {(x,y): float('inf') for x in range(n) for y in range(n)}
+    dist[0,0] = 0
 
-dist = {(x,y): float('inf') for x in range(n) for y in range(n)}
-dist[0,0] = 0
+    pq = [(0, (0,0))] # priority queue: distance, coordinates
 
-pq = [(0, (0,0))] # priority queue: distance, coordinates
+    while pq:
+        current_distance, (x,y) = heapq.heappop(pq) # Pop the node with the smallest distance
 
-while pq:
-    current_distance, (x,y) = heapq.heappop(pq) # Pop the node with the smallest distance
+        if current_distance > dist[x,y]: # continue if better distance already found
+            continue
 
-    if current_distance > dist[x,y]: # continue if better distance already found
-        continue
+        # Explore the neighbors of the current node
+        for neighbor in get_neighbors(x,y,memory_space=memory_space):
+            alt = current_distance + 1
 
-    # Explore the neighbors of the current node
-    for neighbor in get_neighbors(x,y):
-        alt = current_distance + 1
+            if alt < dist[neighbor]:
+                dist[neighbor] = alt
+                heapq.heappush(pq, (alt, neighbor))
 
-        if alt < dist[neighbor]:
-            dist[neighbor] = alt
-            heapq.heappush(pq, (alt, neighbor))
+    return dist
 
-print(dist[n-1, n-1]) # part 1: 320
+print(dijkstra(memory_space)[n-1, n-1]) # part 1: 320
+
+
+### part 2
+memory_space = set()
+for i in range(len(coordinates)):
+    a,b = coordinates[i].split(',')
+    a,b = int(a),int(b)
+    memory_space.add((a,b))
+
+    if dijkstra(memory_space)[n-1, n-1] == float('inf'):
+        print(f'{a},{b}') # part 2: 
+        break
